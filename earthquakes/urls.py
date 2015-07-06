097cea62e2526072375bb86398ec258c84ffba26
Christos Bacharakis <christos@bacharakis.com>
2015-07-06 16:20:22 +0300

    Added events API basic functionality

--- earthquakes/urls.py
+++ earthquakes/urls.py
@@ -5,6 +5,7 @@ from earthquakes import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^stations$', views.stations_api, name='stations_api')
+    url(r'^events$', views.events_api, name='events_api')
 
 
 )
--- earthquakes/views.py
+++ earthquakes/views.py
@@ -26,6 +26,30 @@ def stations_api(request):
 
     return render(request, 'earthquakes/index.html')
 
+def events_api(request):
+
+    if request.method == 'GET':
+        #POST goes here . is_ajax is must to capture ajax requests. Beginner's pit.
+
+        #Always use get on request.POST. Correct way of querying a QueryDict.
+        name = request.GET.get('name')
+        code = request.GET.get('code')
+        fi = request.GET.get('fi')
+        lamda = request.GET.get('lamda')
+        height = request.GET.get('height')
+
+        data = serializers.serialize('json', stations.objects.filter(station_name__icontains=name)\
+        .filter(station_code__icontains=code)\
+        .filter(fi__icontains=fi)\
+        .filter(lamda__icontains=lamda)\
+        .filter(height__icontains=height) )
+
+
+        return JsonResponse(data, safe=False)
+
+    return render(request, 'earthquakes/index.html')
+
+
 
 def search(req):
     if req.GET:
