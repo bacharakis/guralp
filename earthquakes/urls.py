ceb50198e258e33f9fee67d5ab2745de0d0fa325
Christos Bacharakis <christos@bacharakis.com>
2015-07-06 10:52:50 +0300

    Added API functionality

--- earthquakes/urls.py
+++ earthquakes/urls.py
@@ -4,6 +4,7 @@ from earthquakes import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^guralp$', views.api_calls, name='guralp')
 
 
 )
--- earthquakes/views.py
+++ earthquakes/views.py
@@ -1,1 +1,21 @@
-# Create your views here.
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
+from earthquakes.models import stations
+from django.core import serializers
+
+def api_calls(request):
+
+    if request.method == 'GET':
+        #POST goes here . is_ajax is must to capture ajax requests. Beginner's pit.
+
+        #Always use get on request.POST. Correct way of querying a QueryDict.
+        name = request.GET.get('name')
+        code = request.GET.get('code')
+        stations_list = stations.objects.filter(station_code=code)
+
+        #need to serialize the model before sending the result of the request
+        data = serializers.serialize('json', stations.objects.filter(station_code=code))
+
+        return JsonResponse(data, safe=False)
+
+    return render(request, 'earthquakes/index.html')
