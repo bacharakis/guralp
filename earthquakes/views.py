fd62a30b9007a34d5dd1688606f49337d09064c5
Christos Bacharakis <christos@bacharakis.com>
2015-07-22 11:33:50 +0300

    Added [, and ,] search for events

--- earthquakes/views.py
+++ earthquakes/views.py
@@ -88,9 +88,18 @@ def events_api(request):
 
         if eventLowDepth and eventHighDepth:
             filteredEvents = filteredEvents.filter(depth__gte=eventLowDepth, depth__lte=eventHighDepth)
+        elif eventLowDepth and not eventHighDepth:
+            filteredEvents = filteredEvents.filter(depth__gte=eventLowDepth)
+        elif eventHighDepth and not eventLowDepth:
+            filteredEvents = filteredEvents.filter(depth__lte=eventHighDepth)
 
         if eventLowMMF and eventHighMMF:
             filteredEvents = filteredEvents.filter(mmf__gte=eventLowMMF, mmf__lte=eventHighMMF)
+        elif eventLowMMF and not eventHighMMF:
+            filteredEvents = filteredEvents.filter(mmf__gte=eventLowMMF)
+        elif eventHighMMF and not eventLowMMF:
+            filteredEvents = filteredEvents.filter(mmf__lte=eventHighMMF)
+
 
         #Distance (in km from the center of the map to the edges
         zoomlevel = array("i",[1000000, 100000, 10000 , 1000 , 600, 500 , 400 ,300 , 170, 100, 80, 60, 50, 40, 30, 10, 5, 4, 3, 2, 1, 1])
