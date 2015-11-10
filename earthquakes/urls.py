4d54d9f8491d4dd93f33f078215a25a4c8641cdc
Christos Bacharakis <christos@bacharakis.com>
2015-11-10 05:04:11 +0200

    Added necessary URL paths to match new views

--- earthquakes/urls.py
+++ earthquakes/urls.py
@@ -5,7 +5,14 @@ from earthquakes import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^stations$', views.stations_api, name='stations_api'),
-    url(r'^events$', views.events_api, name='events_api')
+    url(r'^events$', views.events_api, name='events_api'),
+    url(r'^plot$', views.plotting_files, name='plotting_files'),
+    url(r'^plot_station$', views.plotting_station_files, name='plotting_station_files'),
+    url(r'^get_files$', views.get_files_json, name='get_files_json'),
+    url(r'^charts$', views.linechart),
+    url(r'^get_filenames$', views.get_filenames, name='get_filenames'),
+    url(r'^download$', views.download_files),
+    url(r'^chart.html', views.chart),
 
 
 )
